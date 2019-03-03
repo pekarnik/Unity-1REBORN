c8822b66aa9bb0a6d82861a91a340989b27b53f1
Pekarnik <pekgoglo@gmail.com>
2019-03-03 03:49:17 +0300

    lesson 2 and full lesson1

---
+++ FPS/Assets/Scripts/Model/Bullet.cs
@@ -0,0 +1,13 @@
+﻿namespace FPS
+{
+	public class Bullet:Ammunition
+	{
+		private void OnCollisionEnter(UnityEngine.Collision collision)
+		{
+			var tempObj = collision.gameObject.GetComponent<ISetDamage>();
+			int criticalDamage=UnityEngine.Random.Range(2,5);
+			tempObj?.SetDamage(new InfoCollision(_curDamage*UnityEngine.Random.Range(0,3)>UnityEngine.Random.Range(0,100)?criticalDamage:1, Rigidbody.velocity));
+			Destroy(gameObject);
+		}
+	}
+}
